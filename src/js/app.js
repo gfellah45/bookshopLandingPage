8ed64b9e256d31845d9dba4689f48ed4a4d54147
gfellah45 <jerry.aaron45@gmail.com>
2020-05-13 11:17:34 +0100

    handleed horizontal scrolling on small screens

--- src/js/app.js
+++ src/js/app.js
@@ -6,3 +6,13 @@ hambuger.addEventListener("click", () => {
 	nav.classList.toggle("show");
 	hambuger.classList.toggle("change");
 });
+
+window.addEventListener("scroll", () => {
+	let scrollPos = window.scrollY;
+
+	if (scrollPos > 90) {
+		nav.classList.add("handle-scroll");
+	} else {
+		nav.classList.remove("handle-scroll");
+	}
+});
